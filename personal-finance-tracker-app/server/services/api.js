f4c5f780c2144a50662a5edbc56cd7e68d99800d
zuhairm2001 <zuhairm2001@hotmail.com>
2024-07-14 18:31:46 +1000

    created the service file

---
+++ personal-finance-tracker-app/server/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:4000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const userService = {
+    createUser: (userData) => api.post('/users', userData),
+    getUsers: () => api.get('/users'),
+    updateUser: (id, userData) => api.put(`/users/${id}`, userData),
+    deleteUser: (id) => api.delete(`/users/${id}`),
+  };
+  
+  export default api;
+  
