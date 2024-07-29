63849e0274436ddb0493626e7fe08e9034785930
zuhairm2001 <zuhairm2001@hotmail.com>
2024-07-29 11:49:29 +1000

    tmp

--- personal-finance-tracker-app/server/controllers/userController.js
+++ personal-finance-tracker-app/server/controllers/userController.js
@@ -1,9 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-<<<<<<< HEAD
-=======
 const cookieParser = require("cookie-parser");
->>>>>>> jwtimp
 const { createTokens } = require("../config/JWT");
 
 exports.createUser = async (req, res) => {
@@ -95,47 +92,6 @@ exports.loginUser = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
-// exports.loginUser = async (req, res) => {
-//   console.log("Login request received");
-//
-//   const { email, password } = req.body;
-//   console.log(`Email: ${email}, Password: ${password}`);
-//
-//   try {
-//     const user = await User.findOne({ email: email });
-//     console.log("User found:", user);
-//
-//     if (!user) {
-//       console.log("User doesn't exist");
-//       return res.status(400).json({ error: "User doesn't exist" });
-//     }
-//
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     console.log("Password match:", isMatch);
-//
-//     if (!isMatch) {
-//       console.log("Invalid credentials");
-//       return res.status(400).json({ error: "Invalid credentials" });
-//     }
-//
-//     console.log("Generating token...");
-//     // const token = generateToken(user);
-//
-//     res.json({
-//       message: "Login successful",
-//       user: {
-//         id: user._id,
-//         email: user.email,
-//         name: user.name,
-//       },
-//       // token: token
-//     });
-//     console.log("Login response sent");
-//   } catch (error) {
-//     console.error("Server error:", error);
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 exports.deleteUser = async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
@@ -148,128 +104,6 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-exports.loginUser = async (req, res) => {
-  console.log("Login request received");
-
-  const { email, password } = req.body;
-  console.log(`Email: ${email}, Password: ${password}`);
-
-  try {
-    const user = await User.findOne({ email: email });
-    console.log("User found:", user);
-
-    if (!user) {
-      console.log("User doesn't exist");
-      return res.status(400).json({ error: "User doesn't exist" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    console.log("Password match:", isMatch);
-
-    if (!isMatch) {
-      console.log("Invalid credentials");
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
-
-    // Token generation placeholder
-    console.log("Generating token...");
-    // const token = generateToken(user);
-
-    res.json({
-      message: "Login successful",
-      user: {
-        id: user._id,
-        email: user.email,
-        name: user.name,
-      },
-      // token: token
-    });
-    console.log("Login response sent");
-  } catch (error) {
-    console.error("Server error:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-};
-
-// exports.loginUser = async (req, res) => {
-//   console.log("Login attempt received:", req.body.email);
-//   const { email, password } = req.body;
-//
-//   if (!email || !password) {
-//     console.log("Login failed: Missing email or password");
-//     return res.status(400).json({ error: "Email and password are required" });
-//   }
-//
-//   try {
-//     console.log("Finding user in database...");
-//     const user = await User.findOne({ email: email });
-//     if (!user) {
-//       console.log("Login failed: User not found");
-//       return res.status(400).json({ error: "User doesn't exist" });
-//     }
-//
-//     console.log("User found, comparing passwords...");
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       console.log("Login failed: Invalid credentials");
-//       return res.status(400).json({ error: "Invalid credentials" });
-//     }
-//
-//     console.log("Password match, creating token...");
-//     const accessToken = createTokens(user);
-//
-//     console.log("Setting cookie and sending response...");
-//     res.cookie("access-token", accessToken, {
-//       maxAge: 2592000000, // 30 days
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: "strict",
-//     });
-//
-//     return res.json({
-=======
-// exports.loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-//
-//   try {
-//     const user = await User.findOne({ email: email });
-//     if (!user) {
-//       return res.status(400).json({ error: "User doesn't exist" });
-//     }
-//
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(400).json({ error: "Invalid credentials" });
-//     }
-//
-//     // TODO: Generate a token here
-//     // const token = generateToken(user);
-//
-//     res.json({
->>>>>>> jwtimp
-//       message: "Login successful",
-//       user: {
-//         id: user._id,
-//         email: user.email,
-//         name: user.name,
-//       },
-<<<<<<< HEAD
-//     });
-//   } catch (error) {
-//     console.error("Login error:", error);
-//     return res.status(500).json({ error: "Server error" });
-//   }
-// };
-
-=======
-//       // token: token
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
->>>>>>> jwtimp
 exports.getUserProfile = async (req, res) => {
   res.json("profile");
 };
