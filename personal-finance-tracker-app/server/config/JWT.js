28e44e1cc1cebd5d583a6345e09a95075eb61c65
zuhairm2001 <zuhairm2001@hotmail.com>
2024-07-29 13:09:38 +1000

    add validate token middleware

--- personal-finance-tracker-app/server/config/JWT.js
+++ personal-finance-tracker-app/server/config/JWT.js
@@ -8,4 +8,21 @@ const createTokens = (user) => {
   return accessToken;
 };
 
+const validateToken (req, res, next) => {
+  const accessToken = req.cookies["accessToken"]
+
+  if (!accessToken) 
+    return res.status(400).json({error: "User not Authenticated"});
+
+  try {
+    const validToken = verify(accessToken, "jwtscretplschange")
+    if (validToken) {
+      req.authenticated = true
+      return next()
+    }
+  } catch(err) {
+    return res.status(400).json({error: err})
+  }
+}
+
 module.exports = { createTokens };
