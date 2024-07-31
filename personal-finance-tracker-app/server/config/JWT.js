d651ce0c41c716346bba905b1e2d4eeb4cd44b22
zuhairm2001 <zuhairm2001@hotmail.com>
2024-07-31 13:37:43 +1000

    validte token works, get profile route works

--- personal-finance-tracker-app/server/config/JWT.js
+++ personal-finance-tracker-app/server/config/JWT.js
@@ -1,21 +1,26 @@
 const { sign, verify } = require("jsonwebtoken");
+require("dotenv").config({ path: ".env" });
 
 const createTokens = (user) => {
   const accessToken = sign(
     { username: user.username, id: user.id },
-    "jwtscretplschange",
+    process.env.JWT_SECRET,
   );
   return accessToken;
 };
 
 const validateToken = (req, res, next) => {
-  const accessToken = (req.cookies.title = "accessToken");
+  const accessToken = req.cookies["accessToken"];
 
   if (!accessToken)
     return res.status(400).json({ error: "User not Authenticated" });
 
   try {
-    const validToken = verify("accessToken", "jwtscretplschange");
+    // res.json({
+    //   accesstoken: accessToken,
+    // });
+    const validToken = verify(accessToken, process.env.JWT_SECRET);
+
     console.log(validToken);
     if (validToken) {
       req.authenticated = true;
