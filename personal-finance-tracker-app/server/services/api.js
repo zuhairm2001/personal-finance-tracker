b345f6a8289e6736d75f32b36ce55dfaf423b78a
zuhairm2001 <zuhairm2001@hotmail.com>
2024-08-02 00:01:25 +1000

    working on restful imp for frontend

--- personal-finance-tracker-app/server/services/api.js
+++ personal-finance-tracker-app/server/services/api.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const userService = {
-    createUser: (userData) => api.post('/users', userData),
-    getUsers: () => api.get('/users'),
-    updateUser: (id, userData) => api.put(`/users/${id}`, userData),
-    deleteUser: (id) => api.delete(`/users/${id}`),
-  };
-  
-  export default api;
-  
+  createUser: (userData) => api.post("/users", userData),
+  getUsers: () => api.get("/users"),
+  updateUser: (id, userData) => api.put(`/users/${id}`, userData),
+  deleteUser: (id) => api.delete(`/users/${id}`),
+};
+
+export default api;
