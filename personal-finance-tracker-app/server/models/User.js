d18f459d27b0c4c84ed6b0f01bd0100e4595551c
zuhairm2001 <zuhairm2001@hotmail.com>
2024-08-04 17:42:56 +1000

    unsaved changes for prev commit

--- personal-finance-tracker-app/server/models/User.js
+++ personal-finance-tracker-app/server/models/User.js
@@ -1,41 +1,51 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
     trim: true,
-    lowercase: true
+    lowercase: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  connectedBanks: [
+    {
+      bankId: String,
+      accountId: String,
+      accountName: String,
+      accountType: String,
+      balance: Number,
+      currency: String,
+      lastUpdated: Date,
+    },
+  ],
+  lastSyncDate: Date,
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 8);
   next();
 });
 
 // Method to check password
-UserSchema.methods.checkPassword = async function(password) {
+UserSchema.methods.checkPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
-
-
+module.exports = mongoose.model("User", UserSchema);
